4ac17a655592a91ebf65e1d72a7565c7d927de25
npolyak <nick.polyak@verizon.net>
2023-01-10 13:36:59 -0500

    moved messages to RelayService for simplification

---
+++ src/MessageHelper.cs
@@ -0,0 +1,17 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using NP.Protobuf;
+
+namespace NP.Grpc.CommonRelayInterfaces
+{
+    public static class MessageHelper
+    {
+        public static ShortMsg ToShortMsg(this System.Enum topic)
+        {
+            return new ShortMsg
+            {
+                MsgSentTime = Timestamp.FromDateTime(DateTime.UtcNow),
+                Topic = topic.ToEnumVal()
+            };
+        }
+    }
+}
